5821d6ac9d4d1afd6dfcd31e98ac4cb9125121bf
Tolunay Hos <tolunayhos@gmail.com>
2022-01-17 21:09:21 +0100

    fetching data as json with REST enabled

--- incidents.js
+++ incidents.js
@@ -1,24 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const Incident = require("./modals/IncidentModel");
+const mongoose = require("mongoose");
+
+const IncidentModel = mongoose.model("IncidentReport");
 
 router.get("/", (req, res) => {
-  res.send("We are on incidents");
+  IncidentModel.find((err, incidents) => {
+    if (!err) {
+      res.json({
+        data: incidents,
+      });
+    } else {
+      console.log("failed to retrieve");
+      res.json({
+        message: err.message,
+        error: err,
+      });
+    }
+  });
 });
 
-router.post("/", (req, res) => {
-  const incident = new Incident({
-    datetime: req.body.datetime,
-    name: req.body.name,
-    summary: req.body.summary,
-    location: {
-      name: req.body.location.name,
-      gps: req.body.location.gps,
-    },
-  });
-  incident.save().then((data) => {
-    res.json(data);
-  });
-});
+// router.post("/", (req, res) => {
+//   const incident = new Incident({
+//     datetime: req.body.datetime,
+//     name: req.body.name,
+//     summary: req.body.summary,
+//     location: {
+//       name: req.body.location.name,
+//       gps: req.body.location.gps,
+//     },
+//   });
+//   incident.save().then((data) => {
+//     res.json(data);
+//   });
+// });
 
 module.exports = router;
